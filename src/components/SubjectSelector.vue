3cdd23751b1e7f48a6dece466e6c77a852f2a61c
Adam Stirtan <adamstirtan@gmail.com>
2023-02-22 15:05:46 -0500

    Fixes

--- src/components/SubjectSelector.vue
+++ src/components/SubjectSelector.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="grid-container">
-        <button type="button" class="grid-button" @click="$emit('changeSubject', 'movies')">🍿 Movies</button>
         <button type="button" class="grid-button" @click="$emit('changeSubject', 'frontend-development')">👨‍💻 Frontend Development</button>
-        <button type="button" class="grid-button" @click="$emit('changeSubject', 'video-games')">🎮 Video Games</button>
         <button type="button" class="grid-button" @click="$emit('changeSubject', 'backend-development')">🧑‍🔬 Backend Development</button>
+        <button type="button" class="grid-button" @click="$emit('changeSubject', 'video-games')">🎮 Video Games</button>
+        <button type="button" class="grid-button" @click="$emit('changeSubject', 'movies')">🍿 Movies</button>
         <button type="button" class="grid-button" @click="$emit('changeSubject', 'math')">➗ Math</button>
         <button type="button" class="grid-button" @click="$emit('changeSubject', 'random')">🎲 Random</button>
     </div>
